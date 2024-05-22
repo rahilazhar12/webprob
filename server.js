21810b8fee83b7f213a817642743f53db4a2c4f8
rahilazhar12 <158289635+rahilazhar12@users.noreply.github.com>
2024-05-22 18:44:19 +0500

    Update server.js

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ const markAbsentees = async () => {
 //     }
 //   });
 
-cron.schedule('43 18 * * *', async () => {
+cron.schedule('01 15 * * *', async () => {
     console.log("Running the absentee marking job...");
     try {
         await markAbsentees();
